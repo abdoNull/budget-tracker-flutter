7990a92607a48be03b51f94ef28f08c9a7d199a8
abderrahmen-bmz <abdogray@gmail.com>
2020-09-21 09:48:42 +0100

    add library index

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:budget_tracker/screens/index.dart';
 
-final routes = {
-  '/': (BuildContext context) => HomePage(),
-  '/accounts': (BuildContext context) => AccountsPage(),
-  '/items': (BuildContext context) => ItemsPage(),
-  '/types': (BuildContext context) => TypesPage(),
-};
+// final routes = {
+//   '/': (BuildContext context) => HomePage(),
+//   '/accounts': (BuildContext context) => AccountsPage(),
+//   '/items': (BuildContext context) => ItemsPage(),
+//   '/types': (BuildContext context) => TypesPage(),
+// };
---
+++ lib/screens/index.dart
@@ -0,0 +1,10 @@
+library pages;
+
+export './home/home_screen.dart';
+export './accounts/account_screen.dart';
+export './accounts/accounts_screen.dart';
+export './icons/icons_screen.dart';
+export './items/item_screen.dart';
+export './items/items_screen.dart';
+export './types/type_screen.dart';
+export './types/types_screen.dart';
